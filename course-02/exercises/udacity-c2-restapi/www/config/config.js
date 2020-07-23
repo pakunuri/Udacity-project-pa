42b59eda09df00e5f35a1a173d5ef22119a245e2
pakunuri <67881940+pakunuri@users.noreply.github.com>
2020-07-22 21:24:03 -0400

    Add files via upload

---
+++ course-02/exercises/udacity-c2-restapi/www/config/config.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "dev": {
+        "username": "udagramakudev",
+        "password": "$Udagram_2020",
+        "database": "udagramakudev",
+        "host": "udagramakudev.cu1hzbxc8icj.us-east-1.rds.amazonaws.com",
+        "dialect": "postgres",
+        "aws_region": "us-east-1",
+        "aws_profile": "default",
+        "aws_media_bucket": "udagramakudev"
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    }
+};
+//# sourceMappingURL=config.js.map
---
+++ course-02/exercises/udacity-c2-restapi/www/config/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramakudev",
+    "password": "$Udagram_2020",
+    "database": "udagramakudev",
+    "host": "udagramakudev.cu1hzbxc8icj.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagramakudev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}

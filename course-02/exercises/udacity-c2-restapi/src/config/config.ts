d4796a7919d2e87d821f80c686f3ece190a09e51
pakunuri <67881940+pakunuri@users.noreply.github.com>
2020-07-22 20:30:28 -0400

    RESTAPI changes

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramakudev",
-    "password": "$Udagram_2020",
-    "database": "udagramakudev",
-    "host": "udagramakudev.cu1hzbxc8icj.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,//"udagramakudev",
+    "password": process.env.POSTGRES_PASSWORD,//"$Udagram_2020",
+    "database": process.env.POSTGRES_DATABASE,//"udagramakudev",
+    "host": process.env.POSTGRES_HOST,//"udagramakudev.cu1hzbxc8icj.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagramakudev"
+    "aws_region": process.env.AWS_REGION,//"us-east-1",
+    "aws_profile": process.env.AWS_PROFILE,//"default",
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET//"udagramakudev"
   },
   "prod": {
     "username": "",

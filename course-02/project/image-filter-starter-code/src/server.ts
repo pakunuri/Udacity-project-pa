74fc079dad0e51ed6f331f1660a303e7e13cd9a0
pakunuri <67881940+pakunuri@users.noreply.github.com>
2020-08-20 19:12:17 -0400

    image changes 2

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -37,20 +37,18 @@ import { Router, Request, Response } from 'express';
       var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
       console.log("full url is " +fullUrl);
       var image_url = req.query.image_url;
-
-      if(!image_url){
-
-      return res.status(400).send('invalid image url');
-
-    }
-
-    else{ 
+      if(!image_url)
+       {
+        res.status(400).send('Invalid URL');
+       }
+      else
+       { 
         var image = await filterImageFromURL(image_url);
         res.status(200).sendFile(image, function(err){
-        if(!err){
-                let filesToDelete = [image];
-                deleteLocalFiles(filesToDelete);
-                }
+        if(!err)
+         {
+           deleteLocalFiles([image]);
+         }
       });
     }
   });
